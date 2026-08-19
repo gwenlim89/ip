6b0c7dfb495146620cb676375b52d25150cea40a
gwenlim89 <gwen.lim.h.p@gmail.com>
2026-08-19 20:02:19 +0800

    level 0

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [to be filled]
-* IDE and level of expertise: [to be filled]
+* Level of programming experience: Low level
+* IDE and level of expertise: Substantial 
 
 # Guidance for interacting with users
 
---
+++ src/main/java/Larper.java
@@ -0,0 +1,20 @@
+public class Larper {
+    public static void main(String[] args) {
+
+        System.out.println("____________________________________________________________ \n");
+        String banner = " _                              \n"
+                + "| |       __ _   _ __   _ __     ___   _ __\n"
+                + "| |      / _` | | '__| | '_ \\   / _ \\ | '__|\n"
+                + "| |___  | (_| | | |    | |_) | |  __/ | |\n"
+                + "|_____|  \\__,_| |_|    | .__/   \\___| |_|\n"
+                + "                       |_|\n";
+        System.out.println(banner);
+        System.out.println("Fine day! I'm Larper. \n");
+        System.out.println("What can I do for you? \n");
+        System.out.println("____________________________________________________________ \n");
+        System.out.println("Bye. Hope to see you soon! \n");
+        System.out.println("____________________________________________________________ \n");          
+
+
+    }
+}
