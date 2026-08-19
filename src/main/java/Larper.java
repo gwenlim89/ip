88daf3c82328c4bfed272481cb02c63ea9f19108
gwenlim89 <gwen.lim.h.p@gmail.com>
2026-08-19 20:48:09 +0800

    level 1

--- src/main/java/Larper.java
+++ src/main/java/Larper.java
@@ -1,23 +1,38 @@
+import java.util.Scanner;
+
 public class Larper {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
 
-        System.out.println("____________________________________________________________ \n");
+        System.out.println(line);
         String banner = " _                              \n"
                 + "| |       __ _   _ __   _ __     ___   _ __\n"
                 + "| |      / _` | | '__| | '_ \\   / _ \\ | '__|\n"
                 + "| |___  | (_| | | |    | |_) | |  __/ | |\n"
                 + "|_____|  \\__,_| |_|    | .__/   \\___| |_|\n"
                 + "                       |_|\n";
-        System.out.println(banner);
+        System.out.print(banner);
         System.out.println("Fine day! I'm Larper. \n");
-        System.out.println("What can I do for you? \n");
-        
+        System.out.println(" What can I do for you? \n");
+        System.out.println(line);
 
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("____________________________________________________________ \n");
-        System.out.println("Bye. Hope to see you soon! \n");
-        System.out.println("____________________________________________________________ \n");          
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
 
+            System.out.println(line);
 
+            if (input.equals("exit")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(" " + input);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
